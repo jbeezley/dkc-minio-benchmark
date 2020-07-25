a8c9bf320e672b4e5d8b9e6908b154bad0674820
Jonathan Beezley <jonathan.beezley@kitware.com>
2020-07-25 00:11:21 -0400

    Add main user to docker group

--- minio-server.sh
+++ minio-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get -y update
 apt-get -y install docker.io
+usermod -a -G docker ubuntu
 
 mkdir -p /data
 docker run --name minio -d -v $PWD/data:/data -p 9000:9000 -e MINIO_ACCESS_KEY=admin -e MINIO_SECRET_KEY=miniopassword minio/minio server /data
